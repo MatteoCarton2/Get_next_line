54d1f3621166c12c8a5fa7c9764750f7f05f6380
Mattéo <mcarton@student.s19.be>
2025-02-07 11:56:23 +0100

    chgmts

--- get_next_line.c
+++ get_next_line.c
@@ -5,52 +5,107 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 12:49:48 by mcarton           #+#    #+#             */
-/*   Updated: 2024/10/21 22:38:47 by mcarton          ###   ########.fr       */
+/*   Created: 2025/02/05 17:56:16 by mcarton           #+#    #+#             */
+/*   Updated: 2025/02/07 11:55:50 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	int			octets_lu;
-	static char	*restant;
+    int bytes_read;
+    size_t i;
+    size_t j;
+    size_t k;
+    char *buffer;     // on créer le "sceau", le buffer
+    int flag; // temporaire
+    char *line;
+    static char *remaining;
 
-	char buffer[BUFFER_SIZE + 1];
-    octets_lu = read(fd, buffer, BUFFER_SIZE);
-    if (octets_lu == -1)
-        return (NULL);
-    buffer[octets_lu] = '\0';
-
+    i = 0;
+    j = 0;
+    k = 0;
+    flag = 0; // temporaire
+    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // on malloc ce "sceau" avec la taille du sceau défini via BUFFER_SIZE + \0
+    if (!buffer)
+        return (NULL);    
+    bytes_read = read(fd, buffer, BUFFER_SIZE);  // read = remplit le buffer
+    //	•	fd → Le fichier à lire (le descripteur de fichier).
+	//  •	buffer → Là où les données seront stockées (le sceau).
+	//  •	BUFFER_SIZE → Combien d’octets on veut lire à chaque fois(la taille du sceau).
+    if (bytes_read < 1)     // si c'est 0 = fin du fichier, si -1 = erreur
+        return (free(buffer), NULL);
+    buffer[bytes_read] = '\0'; // mettre le \0 à la position bytes_read, car c’est le nombre exact de caractères lus.
+    while (buffer[i])
+    {
+        if(buffer[i] == '\n')
+        {
+            printf("Position trouvée = %zu\n", i);
+            flag = 1;
+            line = malloc(sizeof(char) * (i + 2));
+            if (!line)
+                return (free(buffer), NULL);
+            while (j <= i)
+            {
+                line[j] = buffer[j];
+                j++;
+            }
+            line[j] = '\0';
+            remaining = malloc(sizeof(char) * ());
+            if (!remaining)
+                return(free(line), free(buffer), NULL);
+            while (j < i)
+            {
+                remaining[k] = buffer[j];
+                j++;
+            }
+            remaining[k] = '\0';
+            break;
+        }
+        i++;
+    }
+    if (flag == 0)
+    {
+        printf("Pas de saut de ligne trouvé\n");
+        line = malloc(sizeof(char) * (i + 2));
+        if (!line)
+            return (free(buffer), NULL);
+        while (j < i)
+            {
+                line[j] = buffer[j];
+                j++;
+            }
+        line[j] = '\0';
+    }
+    free(buffer);
+    return (line);
 }
 
-int	main(int argc, char **argv)
-{
-	int fd;
-	char *ligne;
+#include <fcntl.h>
+#include <stdio.h>
 
-	if (argc < 2)
-	{
-		printf("Trop peu d'arguments.\n");
-		return (1);
-	}
-	if (argc > 2)
-	{
-		printf("Trop d'arguments.\n");
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Erreur lors de l'ouverture du fichier.\n");
-		return (1);
-	}
-	while ((ligne = get_next_line(fd)))
-	{
-		printf("%s", ligne);
-		free(ligne);
-	}
-	close(fd);
-	return (0);
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+    
+    if (argc != 2)
+        return (0);
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Erreur lors de l'open du fd\n");
+        return (0);
+    }
+    line = get_next_line(fd);
+    while (line)
+    {
+        printf("%s", line);
+        free(line);
+        line = get_next_line(fd);
+    }
+    close(fd);
+    return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,23 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 12:50:37 by mcarton           #+#    #+#             */
-/*   Updated: 2024/10/21 22:31:40 by mcarton          ###   ########.fr       */
+/*   Created: 2025/02/05 21:23:20 by mcarton           #+#    #+#             */
+/*   Updated: 2025/02/07 11:56:10 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-
-#  include <fcntl.h>
-#  include <stdio.h>
-#  include <stdlib.h>
-#  include <string.h>
-#  include <unistd.h>
-
-char	*get_next_line(int fd);
-
+#  define BUFFER_SIZE 255
 # endif
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
 #endif
